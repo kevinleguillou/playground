abf18646c7c292bd08c96506fff8d06885da9a0e
Kevin Le Guillou <contact@kevinleguillou.com>
2020-02-02 22:48:10 +0100

    Added scroll hint to the calendar tabs

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,4 +1,6 @@
 const dayTabs = document.querySelectorAll(".day-tab");
+let currentTab = document.querySelector(".day-tab.current");
+let scrollHint = document.querySelector(".calendar-scroll-hint");
 
 dayTabs.forEach((tab)=>{
 	tab.addEventListener("click", ()=>{
@@ -6,5 +8,20 @@ dayTabs.forEach((tab)=>{
 			el.classList.remove("current");
 		});
 		tab.classList.add("current");
+		currentTab = tab;
 	});
 });
+
+function loop(){
+	let tabRect = currentTab.getBoundingClientRect();
+	if(tabRect.x+tabRect.width > scrollHint.offsetLeft){
+		scrollHint.style.opacity = 0;
+	}else{
+		scrollHint.style.opacity = 1;
+	}
+	requestAnimationFrame(()=>{
+		loop()
+	});
+}
+
+loop();
